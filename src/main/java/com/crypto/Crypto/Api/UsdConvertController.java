86cabcb84d0e31bca8dc7803abf73f5b9b468a36
Zen1101 <Amelkalac21@gmail.com>
2020-05-24 17:47:15 +0100

    Returning 404 on bad request
    Signed-off-by: Zen1101 <Amelkalac21@gmail.com>

--- src/main/java/com/crypto/Crypto/Api/UsdConvertController.java
+++ src/main/java/com/crypto/Crypto/Api/UsdConvertController.java
@@ -4,6 +4,8 @@ import com.crypto.Crypto.Factory.CryptoCurrencyFactory;
 import com.crypto.Crypto.Abstract.CryptoAbstract;
 import com.crypto.Crypto.Model.CryptoModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +19,15 @@ public class UsdConvertController {
     private CryptoCurrencyFactory cryptoCurrencyFactory;
 
     @GetMapping(path = "/{currency}/usd")
-    public CryptoModel ToUsd(@PathVariable(value = "currency") String currency) {
+    public ResponseEntity<CryptoModel> ToUsd(@PathVariable(value = "currency") String currency) {
         try {
             CryptoAbstract currentCrypto = cryptoCurrencyFactory.getCrypto(currency);
 
-            return currentCrypto.getCryptoInfo();
+            return ResponseEntity.status(HttpStatus.OK).body(currentCrypto.getCryptoInfo());
         } catch (Exception e) {
             e.printStackTrace();
 
-            return new CryptoModel(0, null);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
     }
 }

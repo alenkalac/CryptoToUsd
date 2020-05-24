79078c2064f90242110a617abd3dc080e72578b1
Zen1101 <Amelkalac21@gmail.com>
2020-05-24 19:10:50 +0100

    Changed response property name
    Signed-off-by: Zen1101 <Amelkalac21@gmail.com>

--- src/main/java/com/crypto/Crypto/Model/CryptoModel.java
+++ src/main/java/com/crypto/Crypto/Model/CryptoModel.java
@@ -1,28 +1,12 @@
 package com.crypto.Crypto.Model;
 
 public class CryptoModel {
-    private double priceInUsd;
+    private double price;
 
     private MarginModel margin;
 
-    public CryptoModel(double priceInUsd, MarginModel margin){
-        this.priceInUsd = priceInUsd;
-        this.margin = margin;
-    }
-
-    public double getPriceInUsd() {
-        return priceInUsd;
-    }
-
-    public void setPriceInUsd(double priceInUsd) {
-        this.priceInUsd = priceInUsd;
-    }
-
-    public MarginModel getMargin() {
-        return margin;
-    }
-
-    public void setMargin(MarginModel margin) {
+    public CryptoModel(double price, MarginModel margin) {
+        this.price = price;
         this.margin = margin;
     }
 }
